4d3454109b6d89ae574a4c592b36e34be9df84b1
Phanx <addons@phanx.net>
2014-11-16 04:58:35 -0800

    Add documentation

--- Collect.lua
+++ Collect.lua
@@ -1,10 +1,10 @@
 --[[--------------------------------------------------------------------
 	Bibliophile
 	Stores the contents of in-game books so you can read them later.
-	Copyright (c) 2013-2014 Phanx. All rights reserved.
-	See the accompanying README and LICENSE files for more information.
+	Copyright (c) 2013-2014 Phanx <addons@phanx.net>. All rights reserved.
 	http://www.wowinterface.com/downloads/info-Bibliophile.html
 	http://www.curse.com/addons/wow/bibliophile
+	https://github.com/Phanx/Bibliophile
 ----------------------------------------------------------------------]]
 
 local ADDON, private = ...
---
+++ README.md
@@ -0,0 +1,4 @@
+Bibliophile
+==============
+
+**EN** — Records the contents of in-game books so you can read them later.
