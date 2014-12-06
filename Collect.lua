e7a59ce2df3ea6c143490c3f101826c680bcb34d
Phanx <addons@phanx.net>
2014-12-06 12:20:44 -0800

    Be quiet

--- Collect.lua
+++ Collect.lua
@@ -39,16 +39,15 @@ function Collect:PLAYER_LOGIN()
 end
 
 function Collect:ITEM_TEXT_READY()
-	print("ITEM_TEXT_READY")
+	--print("ITEM_TEXT_READY")
 	if ItemTextGetCreator() then
 		-- Ignore saved mail and other player-created items
 		return
 	end
 
-
 	local title = ItemTextGetItem()
 	local page = ItemTextGetPage()
-	print("Scanning page", page, "of book", title)
+	--print("Scanning page", page, "of book", title)
 
 	local t = self.db[title] or {}
 	t.material = ItemTextGetMaterial()
@@ -57,11 +56,11 @@ function Collect:ITEM_TEXT_READY()
 	t.pages[page] = ItemTextGetText()
 
 	if not ItemTextHasNextPage() then
-		print("Last page")
+		--print("Last page")
 		t.complete = true
 		for i = 1, page do
 			if not t.pages[i] then
-				print("Missing text for page", i)
+				--print("Missing text for page", i)
 				t.complete = nil
 				break
 			end
@@ -75,7 +74,7 @@ function Collect:ITEM_TEXT_READY()
 		local x, y = GetPlayerMapPosition("player")
 		t.locations = t.locations or {}
 		t.locations[zone] = floor(x * 10000 + 0.5) * 1000000 + floor(y * 10000 + 0.5) * 100 + level
-		print("Saved location", t.locations[zone], "in", zone)
+		--print("Saved location", t.locations[zone], "in", zone)
 	end
 
 	self.db[title] = t
